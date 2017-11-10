707d489728f73e75560bcaa7ed1e105e923766c4
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 00:16:56 +0900

    フレックスボックスを追加

---
+++ src/components/layout/FlexBox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    flexDirection: {
+      type: String,
+      default: 'row'
+    },
+    flexWrap: {
+      type: String,
+      default: 'nowrap'
+    },
+    alignItems: {
+      type: String,
+      default: 'stretch'
+    },
+    justifyContent: {
+      type: String,
+      default: 'flex-start'
+    },
+    width: {
+      type: String,
+      default: 'auto'
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    }
+  },
+  computed: {
+    style () {
+      return {
+        'flex-direction': this.flexDirection,
+        'flex-wrap': this.flexWrap,
+        'align-items': this.alignItems,
+        'justify-content': this.justifyContent,
+        width: this.height,
+        height: this.height
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex-box {
+  display: flex;
+}
+</style>
