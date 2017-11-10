36a5ab1eabff789d5827ac4aff253c0c66dc3a10
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 00:16:00 +0900

    テーマ管理用ミックスインを追加

---
+++ src/components/Theme.vue
@@ -0,0 +1,16 @@
+<script>
+import themeProvider from '../theme/themeProvider'
+
+export default {
+  data () {
+    return {
+      theme: themeProvider.getTheme()
+    }
+  },
+  methods: {
+    getTheme (color) {
+      return this.theme[color]
+    }
+  }
+}
+</script>
