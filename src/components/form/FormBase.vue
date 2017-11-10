43adfdad658eaa9bafa7eeef11dbbf6318a126c1
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 00:16:38 +0900

    フォームのベースミックスインを追加

---
+++ src/components/form/FormBase.vue
@@ -0,0 +1,38 @@
+<script>
+import Theme from '~/Theme'
+
+export default {
+  mixins: [Theme],
+  model: {
+    prop: 'value'
+  },
+  props: {
+    value: {
+      required: true
+    },
+    valid: {
+      type: Boolean,
+      default: true
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    updateValue (value) {
+      if (!this.disabled && !this.readOnly) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
