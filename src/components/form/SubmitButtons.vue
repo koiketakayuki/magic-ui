05e84713c23daa9366436d857f2db0e6514549ad
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 16:01:26 +0900

    submitボタン配置用のコンポーネントを実装

---
+++ src/components/form/SubmitButtons.vue
@@ -0,0 +1,38 @@
+<template>
+  <mu-flex-box :justifyContent="justifyContent">
+    <mu-inline-list gap="10px">
+      <slot></slot>
+    </mu-inline-list>
+  </mu-flex-box>
+</template>
+
+<script>
+import MuFlexBox from '~/layout/FlexBox'
+import MuInlineList from '~/layout/InlineList'
+
+export default {
+  props: {
+    position: {
+      type: String,
+      default: 'right'
+    }
+  },
+  computed: {
+    justifyContent () {
+      if (this.position === 'left') {
+        return 'flex-start'
+      }
+
+      if (this.position === 'center') {
+        return 'center'
+      }
+
+      return 'flex-end'
+    }
+  },
+  components: {
+    MuFlexBox,
+    MuInlineList
+  }
+}
+</script>
