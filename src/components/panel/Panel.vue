af335fc0d6fc628d0b6e3b35f8f5a6c634759a49
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 14:49:28 +0900

    パネルを実装

---
+++ src/components/panel/Panel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="panel" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Theme from '~/Theme'
+
+export default {
+  mixins: [Theme],
+  computed: {
+    style () {
+      return {
+        'border-color': this.theme.gray400
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.panel {
+  display: block;
+  border: 1px solid;
+  box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.12), 0 3px 3px 0px rgba(0, 0, 0, 0.2);
+}
+</style>
