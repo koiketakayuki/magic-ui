17ece057ee9bbdb5f12393fe07389ce70d194e0f
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-11 14:08:30 +0900

    コンテナを実装

---
+++ src/components/layout/Container.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container" :style="{ padding, width, height }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: 'auto'
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    },
+    padding: {
+      type: String,
+      default: '20px'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  transition: padding 0.375s;
+}
+</style>
