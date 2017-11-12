3046303750ee132f1161f948749bed6b951541a4
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-12 17:15:34 +0900

    アクティビティバーを実装

---
+++ src/components/menu/Activity.vue
@@ -0,0 +1,42 @@
+<template>
+  <d-icon-button class="activity fa fa-2x"
+                 :title="name"
+                 :class="{ 'activity--active': active }"
+                 :active="active"
+                 default="gray500"
+                 type="white"
+                 @click="onClick"></d-icon-button>
+</template>
+
+<script>
+import DIconButton from '~/button/IconButton'
+
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onClick (e) {
+      this.$emit('click', e)
+    }
+  },
+  components: {
+    DIconButton
+  }
+}
+</script>
+
+<style scoped>
+.activity {
+  padding: 16px;
+}
+</style>
+
+
---
+++ src/components/menu/ActivityBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <d-flex-box :style="{ background: theme.gray700 }"
+                flexDirection="column"
+                alignItems="center"
+                height="100%">
+      <slot></slot>
+    </d-flex-box>
+  </div>
+</template>
+
+<script>
+import Theme from '~/Theme'
+import DIconButton from '~/button/IconButton'
+import DFlexBox from '~/layout/FlexBox'
+
+export default {
+  mixins: [Theme],
+  components: {
+    DIconButton,
+    DFlexBox
+  }
+}
+</script>
