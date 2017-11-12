bc1c942053042c173e016f0f02b0cfad0f47ad09
koiketakayuki <koike.takayuki0907@gmail.com>
2017-11-12 17:12:15 +0900

    サイドバーのヘッダーのコンポーネントを実装

---
+++ src/components/menu/SideBarHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="side-bar__header" :style="{ background: theme.gray600, color: theme.white }">
+    <div class="side-bar__title">
+      <h2 class="heading-lv02"><slot></slot></h2>
+    </div>
+    <div class="side-bar__delete-button">
+      <mu-icon-button class="fa fa-remove fa-lg"
+                      default="white"
+                      type="errorBackground"
+                      @click="close"></mu-icon-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Theme from '~/Theme'
+import MuIconButton from '~/button/IconButton'
+
+export default {
+  mixins: [Theme],
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  },
+  components: {
+    MuIconButton
+  }
+}
+</script>
+
+
+<style scoped>
+.side-bar__header {
+  position: relative;
+  padding: 16px;
+}
+
+.side-bar__delete-button {
+  position: absolute;
+  margin: auto;
+  top: 50%;
+  right: 12px;
+  transform: translateY(-50%)
+}
+
+.side-bar__title {
+  font-size: 14px;
+}
+</style>
